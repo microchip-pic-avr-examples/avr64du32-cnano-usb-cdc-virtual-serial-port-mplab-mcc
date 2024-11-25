07053946390f82a2394039203f10610af5ff7db9
Henrik Moe Arnesen <HenrikMoe.Arnesen@microchip.com>
2024-11-25 11:05:15 +0100

    MPAE-18615: Update example to include power handling and code cleanup

--- avr64du32-cnano-usb-cdc-virtual-serial-port-mplab-mcc.X/main.c
+++ avr64du32-cnano-usb-cdc-virtual-serial-port-mplab-mcc.X/main.c
@@ -5,9 +5,8 @@
  * @brief This file contain an example for running Virtual Serial Port communication over USB for AVR DU
  * @version MAIN Driver Version 1.0.0
  */
-
 /*
-� [2024] Microchip Technology Inc. and its subsidiaries.
+ï¿½ [2024] Microchip Technology Inc. and its subsidiaries.
 
     Subject to your compliance with these terms, you may use Microchip
     software and any derivatives exclusively with Microchip products.
@@ -27,27 +26,53 @@
     THIS SOFTWARE.
 */
 
+// MCC includes
 #include "mcc_generated_files/system/system.h"
 #include "usb_cdc.h"
 #include "usb_cdc_virtual_serial_port.h"
 
-// Status variables
+// Standard library includes
+#include <stdbool.h>
+#include <util/delay.h>
+
+// Number of consecutive equal AC measurements before power is seen as stable
+#define AC_MEASUREMENT_STABLE_COUNT 5U
+
+// USB status variables
 volatile RETURN_CODE_t status = SUCCESS;
 volatile CDC_RETURN_CODE_t cdcStatus = CDC_SUCCESS;
 
-// Data variable
-uint8_t cdcData;
+// Interrupt variable
+volatile bool vbusFlag = false;
+
+// Private function declarations
+void USB_ConnectionHandler(void);
+void USB_PowerHandler(void);
 
 int main(void)
 {
+    // Data variable
+    uint8_t cdcData;
+
     SYSTEM_Initialize();
+
+    // Set callbacks
+    RTC_SetPITIsrCallback(USB_PowerHandler);
 
     while (1)
     {
-        // Running USB handle
-        status = USBDevice_Handle();
+        USB_ConnectionHandler();
 
-        if (SUCCESS == status)
+        // If USB error detected
+        if (SUCCESS != status)
+        {
+            while (1)
+            {
+                LED_Toggle();
+                _delay_ms(500);
+            }
+        }
+        else
         {
             // Tests if DTE is set and USB ready for transfer
             if (true == USB_CDCDataTerminalReady())
@@ -61,24 +86,89 @@ int main(void)
                     {
                         cdcStatus = USB_CDCWrite(cdcData);
                     }
-                    else
-                    {
-                        // CDC buffer empty no data to transmit
-                        cdcStatus = CDC_SUCCESS;
-                    }
                 }
 
                 // Running CDC Virtual Serial Port handler
                 status = USB_CDCVirtualSerialPortHandler();
             }
-            else
-            {
-                ; // No terminal session active
-            }
+        }
+    }
+}
+
+/**
+ * Routine that checks the connectivity of the USB peripheral and start/stops the USB driver when connected/disconnected
+ */
+void USB_ConnectionHandler(void)
+{
+    static volatile bool prevVbusState = false;
+    // Check if VBUS was true last check, indicating that USB was connected
+    if (prevVbusState == true)
+    {
+        // Handle USB Transfers
+        status = USBDevice_Handle();
+    }
+    // Get current status of VBUS
+    bool currentVbusState = vbusFlag;
+    // If changes to USB VBUS state
+    if (currentVbusState != prevVbusState)
+    {
+        // If USB has been connected
+        if (currentVbusState == true)
+        {
+            // Start USB operations
+            status = USB_Start();
+            LED_SetHigh();
         }
         else
         {
-            ; // USB handle error
+            // Stop USB operations
+            status = USB_Stop();
+            LED_SetLow();
+        }
+        // Save state
+        prevVbusState = currentVbusState;
+    }
+}
+
+/**
+ * Interrupt routine used to check VBUS status using the Real-Time Counter overflow interrupt and Analog Comparator
+ */
+void USB_PowerHandler(void)
+{
+    // Helper variables for RTC and AC
+    static volatile uint8_t acStateCounter = 0U;
+    static volatile uint8_t acPrevState = 0U;
+
+    uint8_t acCurrentState = AC0_Read(); // Saves the current state of the AC status register ('1' means above threshold)
+
+    // Makes sure that the AC state has been the same for a certain number of PIT interrupts
+    if (acStateCounter == AC_MEASUREMENT_STABLE_COUNT)
+    {
+        if (acCurrentState) // When CMPSTATE is high, the AC output is high which means the VBUS is above the threshold and the pull-up on data+ should be enabled.
+        {
+            vbusFlag = true;
+        }
+        else
+        {
+            vbusFlag = false;
+        }
+        acStateCounter++; // Increment the counter to not run start/stop multiple times without an actual state change
+    }
+    // Increments a counter if the AC has been in the same state for a certain number of PIT interrupts
+    else if (acCurrentState == acPrevState)
+    {
+        if (acStateCounter < AC_MEASUREMENT_STABLE_COUNT)
+        {
+            acStateCounter++;
+        }
+        else
+        {
+            ; // Stops counting if the AC has been in the same state for a while.
         }
     }
+    else
+    {
+        acStateCounter = 0; // Resets the PIT counter if a state change has occurred.
+    }
+    acPrevState = acCurrentState;
 }
